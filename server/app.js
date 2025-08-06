dd77b93f8193e4b63396ffd0f13e9412d04a0036
Jimena Sánchez <mjsp674@hotmail.com>
2025-08-06 12:04:25 -0500

    Se configura app.js, db.js y .env para desplegar el backend.

--- server/app.js
+++ server/app.js
@@ -14,17 +14,11 @@ const app = express(); // Crear una instancia de express
 dotenv.config(); // Traer las variebles de entorno de env
 
 const corsOptions = {
-  origin: process.env.CORS_ORIGIN || "*",
+  origin: process.env.CORS_ORIGIN,
   credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"], // Permitir peticiones desde otro origen
 };
-
 app.use(cors(corsOptions));
-
-app.use(
-  cors({
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-); // Permitir peticiones desde otro origen
 
 app.use(express.json()); // Parsear las request en formato JSON
 app.use(express.urlencoded({ extended: true }));
--- server/config/db.js
+++ server/config/db.js
@@ -5,17 +5,19 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 // Conexión a la base de datos
-const connection = db.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+const connection = db.createConnection(
+  process.env.DATABASE_URL || {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  }
+);
 
 connection.connect((err) => {
   if (err) {
     console.error("Error al conectarse a la base de datos.", err);
-    return;
+    process.exit(1);
   }
   console.log("Conectado a la base de datos.");
 });
